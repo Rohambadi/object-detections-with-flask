c549d98d1d77c4f0d97dd846e01c148abb8d0137
rohamm <rohambadi547@gmail.com>
2021-09-27 04:14:01 -0700

    real time object detections with yolo & flask

---
+++ camera.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+coco_file = "coco.names"
+coco_classes = []
+net_config = "cfg/yolov3.cfg"
+net_weights = "cfg/yolov3.weights"
+blob_size = 320
+confidence_threshold = 0.5
+nms_threshold = 0.3
+
+with open(coco_file, "rt") as f:
+    coco_classes = f.read().rstrip("\n").split("\n")
+
+net = cv2.dnn.readNetFromDarknet(net_config, net_weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+class Video(object):
+    def __init__(self):
+        self.video=cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+
+        success, frame = self.video.read()
+        blob = cv2.dnn.blobFromImage(frame, scalefactor=1 / 255, size=(blob_size, blob_size), mean=(0, 0, 0),
+                                         swapRB=True, crop=False)
+        net.setInput(blob)
+
+        out_names = net.getUnconnectedOutLayersNames()
+        output = net.forward(out_names)
+
+        findObjects(output, frame)
+
+        success, jpg=cv2.imencode('.jpg', frame)
+
+        return jpg.tobytes()
+
+
+def findObjects(output, img):
+
+    img_h, img_w, img_c = img.shape
+    bboxes = []
+    class_ids = []
+    confidences = []
+
+    for cell in output:
+        for detect_vector in cell:
+            scores = detect_vector[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > confidence_threshold:
+                w, h = int(detect_vector[2] * img_w), int(detect_vector[3] * img_h)
+                x, y = int((detect_vector[0] * img_w) - w / 2), int((detect_vector[1] * img_h) - h / 2)
+                bboxes.append([x, y, w, h])
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+    indices = cv2.dnn.NMSBoxes(bboxes, confidences, confidence_threshold, nms_threshold)
+    print(indices)
+    for i in indices:
+        i = i[0]
+        bbox = bboxes[i]
+        x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        cv2.putText(img, f'{coco_classes[class_ids[i]].upper()} {int(confidences[i] * 100)}%',
+                    (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+
